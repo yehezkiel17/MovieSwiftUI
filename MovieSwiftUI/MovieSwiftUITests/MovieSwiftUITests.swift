5d4b8e5239d612a75d5cdf1455f4196a502aae2b
Yehezkiel Litbagay <kaka.yehezkiel@gmail.com>
2020-09-29 02:04:15 +0700

    Create new project

---
+++ MovieSwiftUI/MovieSwiftUITests/MovieSwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  MovieSwiftUITests.swift
+//  MovieSwiftUITests
+//
+//  Created by Yehezkiel Litbagay on 29/09/20.
+//  Copyright © 2020 kiel. All rights reserved.
+//
+
+import XCTest
+@testable import MovieSwiftUI
+
+class MovieSwiftUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
