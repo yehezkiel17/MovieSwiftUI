e9657c946bbba372bf1dff7bbb5c2060030139a0
Yehezkiel Litbagay <kaka.yehezkiel@gmail.com>
2020-10-01 01:03:21 +0700

    Add base url & API key

---
+++ MovieSwiftUI/MovieSwiftUI/Utils/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  MovieSwiftUI
+//
+//  Created by Yehezkiel Litbagay on 01/10/20.
+//  Copyright © 2020 kiel. All rights reserved.
+//
+
+import Foundation
+
+enum Constant {
+	
+	static let baseUrl = "https://api.themovie.org"
+	static let apiKey = "API_KEY"
+}
