331009eb6bc7a6aa8e831c9c29a52b5a830e1984
Yehezkiel Litbagay <kaka.yehezkiel@gmail.com>
2020-10-04 03:03:39 +0700

    Add the dummy movie

--- MovieSwiftUI/MovieSwiftUI/Models/Movie.swift
+++ MovieSwiftUI/MovieSwiftUI/Models/Movie.swift
@@ -24,3 +24,22 @@ struct Movie: Codable {
 	let posterPath: String?
 	let backdropPath: String?
 }
+
+extension Movie {
+	static var dummy: Movie {
+		Movie(id: 337401,
+			  title: "Mulan",
+			  overview: "When the Emperor of China issues a decree that one man per family must serve in the Imperial Chinese Army to defend the country from Huns, Hua Mulan, the eldest daughter of an honored warrior, steps in to take the place of her ailing father. She is spirited, determined and quick on her feet. Disguised as a man by the name of Hua Jun, she is tested every step of the way and must harness her innermost strength and embrace her true potential.",
+			  releaseDate: "2020-09-04",
+			  popularity: 1139.529,
+			  voteCount: 2101,
+			  voteAverage: 7.4,
+			  originalLanguage: "en",
+			  originalTitle: "Mulan",
+			  video: false,
+			  adult: false,
+			  genreIds: [28, 12, 18, 14],
+			  posterPath: "/aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
+			  backdropPath: "/zzWGRw277MNoCs3zhyG3YmYQsXv.jpg")
+	}
+}
