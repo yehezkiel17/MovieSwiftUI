c28a55e6660768a7d7b5be2e5ee8303f0d44600d
Yehezkiel Litbagay <kaka.yehezkiel@gmail.com>
2020-10-04 02:06:10 +0700

    Add ImageCache

---
+++ MovieSwiftUI/MovieSwiftUI/Caches/ImageCache.swift
@@ -0,0 +1,44 @@
+//
+//  ImageCache.swift
+//  MovieSwiftUI
+//
+//  Created by Yehezkiel Litbagay on 04/10/20.
+//  Copyright © 2020 kiel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImageCache: ObservableObject {
+	
+	typealias Key = String
+	typealias Value = UIImage
+	
+	static let shared = ImageCache()
+	
+	private let userDefaults = UserDefaults.standard
+	
+	private init() {}
+}
+
+extension ImageCache: Cache {
+	func save(value: Value, key: Key) {
+		let encodedString = value.pngData()?.base64EncodedString()
+		userDefaults.set(encodedString, forKey: key)
+	}
+	
+	func load(key: Key) -> Value? {
+		guard let object = userDefaults.object(forKey: key) as? String,
+			let data = Data(base64Encoded: object) else {
+			return nil
+		}
+		
+		let image = UIImage(data: data)
+		
+		return image
+	}
+	
+	func remove(key: Key) {
+		userDefaults.removeObject(forKey: key)
+	}
+}
