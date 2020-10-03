36efd6aa773e3f35e48613be09c7e6d84b4f03ee
Yehezkiel Litbagay <kaka.yehezkiel@gmail.com>
2020-10-04 00:45:08 +0700

    Add ViewModel and ViewBinding class

---
+++ MovieSwiftUI/MovieSwiftUI/ViewModels/ViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  ViewModel.swift
+//  MovieSwiftUI
+//
+//  Created by Yehezkiel Litbagay on 04/10/20.
+//  Copyright © 2020 kiel. All rights reserved.
+//
+
+import Foundation
+
+protocol ViewModel {}
