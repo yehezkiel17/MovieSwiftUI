056db99529b880dbb38af754cdb9080be0248160
Yehezkiel Litbagay <kaka.yehezkiel@gmail.com>
2020-11-12 00:15:55 +0700

    Add new scheme

---
+++ MovieSwiftUIClips/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MovieSwiftUIClips
+//
+//  Created by Yehezkiel Litbagay on 12/11/20.
+//  Copyright © 2020 kiel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieSwiftUIClips/MovieSwiftUIClipsApp.swift
@@ -0,0 +1,18 @@
+//
+//  MovieSwiftUIClipsApp.swift
+//  MovieSwiftUIClips
+//
+//  Created by Yehezkiel Litbagay on 12/11/20.
+//  Copyright © 2020 kiel. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct MovieSwiftUIClipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
